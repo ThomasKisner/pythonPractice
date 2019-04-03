2075efe3716b9cf75ec1d04cfb6ac5d4f3a7e8f8
thomaskisner <tjkisner@gmail.com>
2019-04-03 12:03:31 -0400

    added clear to calc

--- calculator/main.py
+++ calculator/main.py
@@ -1,23 +1,32 @@
+#import regex
 import re
 print("Our Magical Calculator")
 print("Type 'quit' to exit\n")
 
+#initializing previous total variable
 previous = 0
+#initializing variable which the calc will refer to to see if it should keep running
 run = True
 
 def performMath():
+    #getting run and previous into the function's scope
     global run
     global previous
     equation = ""
 
+    #checking if a calculation has already been run, if not asking for an input
     if previous == 0: 
         equation = input("Enter equation:")
     else: 
         equation = input(str(previous))
 
+    
     if equation == 'quit':
         print('Googbye, human')
         run = False
+    elif equation == 'clear':
+        previous = 0
+        print('cleared\n')
     else: 
         equation = re.sub('[a-zA-Z,.:()" "]', '', equation)
         if previous == 0:
